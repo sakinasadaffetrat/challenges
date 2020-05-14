71d1276aa2204f364e9ac6c87b67fcd90e742117
sakinasadaffetrat <63241096+sakinasadaffetrat@users.noreply.github.com>
2020-05-14 23:00:39 +0200

    Update challenges.js

--- 07-Challenge/challenges.js
+++ 07-Challenge/challenges.js
@@ -1,24 +1,16 @@
 /* Challenges to Solve */
 //  JS Conditions (suite)
-/*Create a function that takes three integer arguments (a, b, c) and returns the amount of integers which are of equal value.*/
-function equal(a, b, c) {
 
-	if ( a == b & b == c)
-	{
-		return 3 + " --> " + " All 3 values are equal";
-	}
-	else if ( a == b || b == c || a == c)
-			{
-				return 2 + " --> " + " Two values are equal";
-			}
-	else
-		{
-			return 0 + " --> " + " All values are differents";
-		}
-		console.logequal(55, 65, 20);
-}
-
-
+function isEvenOrOdd(num) {
+	if (num % 2 === 0){
+		return "Even number !";
+	}else {
+		return "Odd number !"
+	};
+};
+console.log(isEvenOrOdd(146));
+console.log(isEvenOrOdd(19));
+console.log(isEvenOrOdd(3));
 
 
 
